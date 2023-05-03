0f2ed456cf0b834f69acc6edafc25dd0c47e4b67
Zahed Golabi <golabizahed@gmail.com>
2023-05-04 01:01:00 +0430

    script drawing with a mouse

---
+++ 2. Image basics with Opencv/04- Drawing on Images with a mouse.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), 20, color=(255, 0, 0), thickness=-1)
+
+
+cv2.namedWindow("my_drawing")
+cv2.setMouseCallback("my_drawing", draw_circle)
+
+img = np.zeros(shape=(512, 512, 3), dtype=np.int8)
+
+while True:
+
+    cv2.imshow("my_drawing", img)
+
+    if cv2.waitKey(20) & 0xff == 27:
+        break
+
+cv2.destroyAllWindows()
